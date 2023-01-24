b744f6e59ed7e9db922d20d9f79bb475472c9620
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-24 15:24:07 -0300

    Estrutura inicial do projeto.

---
+++ userdept/src/test/java/com/spring/api/rest/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.api.rest.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
