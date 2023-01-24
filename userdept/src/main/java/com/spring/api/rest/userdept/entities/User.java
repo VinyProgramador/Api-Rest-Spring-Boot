8dde69c589ac7d87fe8cad54e70b9c5ec7966390
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-24 17:40:56 -0300

    Utilização de ORM no springboot para desenvolvimento moderno relacional e configuração do gerenciamento do bd.

--- userdept/src/main/java/com/spring/api/rest/userdept/entities/User.java
+++ userdept/src/main/java/com/spring/api/rest/userdept/entities/User.java
@@ -1,11 +1,26 @@
 package com.spring.api.rest.userdept.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
 	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 
 	public long getId() {
