efe5284097062a7a878b64363d0acde77e9f4264
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-24 19:04:57 -0300

    Criação de um EndPoint

--- userdept/src/main/java/com/spring/api/rest/userdept/repositories/UserRepository.java
+++ userdept/src/main/java/com/spring/api/rest/userdept/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.spring.api.rest.userdept.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.spring.api.rest.userdept.entities.User;
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
