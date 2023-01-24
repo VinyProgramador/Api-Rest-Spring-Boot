b550973cb63d9b0db6a5b44af1f11cb7d808f77e
VinyProgramador <vinicius.nunes@sptech.school>
2023-01-24 18:38:25 -0300

    Classe Repository para acessar e fazer as consultas basicas do bd

---
+++ userdept/src/main/java/com/spring/api/rest/userdept/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.spring.api.rest.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
